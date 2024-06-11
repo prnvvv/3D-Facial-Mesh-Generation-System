932ec756d64de7e8c1b0d25d206c02df2e9e09ae
prnvvv <prannavakhanth12@gmail.com>
2024-06-11 15:18:13 +0530

    Update FaceMesh_Project.py

--- FaceMesh_Project.py
+++ FaceMesh_Project.py
@@ -5,17 +5,17 @@ import time
 capture = cv2.VideoCapture(0)
 
 
-
 previousTime = 0
 currentTime = 0
 
 while True:
+    success, vidObject = capture.read()
 
     currentTime = time.time()
     fps = 1 / (currentTime - previousTime)
     previousTime = currentTime
 
-    cv2.imshow()
+    cv2.imshow("Video", vidObject)
 
     if cv2.waitKey(1) == ord("q"):
         break
