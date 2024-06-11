0f15dfeb6be55cf5dc4135a76206af5e6abbfce9
prnvvv <prannavakhanth12@gmail.com>
2024-06-11 15:52:47 +0530

    Update FaceMesh_Project.py

--- FaceMesh_Project.py
+++ FaceMesh_Project.py
@@ -4,6 +4,10 @@ import time
 
 capture = cv2.VideoCapture(0)
 
+mpFaceMesh = mp.solutions.face_mesh
+FaceMesh = mpFaceMesh.FaceMesh(max_num_faces = 2)
+mpDraw = mp.solutions.drawing_utils
+drawSpec = mpDraw.DrawingSpec(thickness = 1, circle_radius = 2)
 
 previousTime = 0
 currentTime = 0
@@ -11,9 +15,23 @@ currentTime = 0
 while True:
     success, vidObject = capture.read()
 
+    imgRGB =    cv2.cvtColor(vidObject, cv2.COLOR_BGR2RGB)
+
+    results = FaceMesh.process(imgRGB)
+
+    detections = results.multi_face_landmarks
+    
+    if detections:
+        for detection in detections:
+            mpDraw.draw_landmarks(vidObject, detection, mpFaceMesh.FACE_CONNECTIONS, drawSpec, drawSpec)
+            for landmark in detection.landmark:
+                print(landmark)
+
     currentTime = time.time()
     fps = 1 / (currentTime - previousTime)
     previousTime = currentTime
+    
+    cv2.putText(vidObject, f'FPS: {int(fps)}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
     cv2.imshow("Video", vidObject)
 
